b42e5fd41b57c1bd2232eae2f4d47b7cd45274aa
Shubham Nhalde <96485743+ShubhamNhalde@users.noreply.github.com>
2022-05-20 17:27:02 +0530

    ADDED: Book Service

---
+++ book/src/main/java/com/bridgelabz/book/BookApplication.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@Slf4j
+public class BookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApplication.class, args);
+		log.info("Book from book store app started");
+
+	}
+}
