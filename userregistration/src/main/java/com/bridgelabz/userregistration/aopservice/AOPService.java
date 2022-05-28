305c75344f8569338f73feef4ea32c2e7f1a2e24
Shubham Nhalde <96485743+ShubhamNhalde@users.noreply.github.com>
2022-05-28 18:37:06 +0530

    Updated: AOP to userregistration service

---
+++ userregistration/src/main/java/com/bridgelabz/userregistration/aopservice/AOPService.java
@@ -0,0 +1,36 @@
+package com.bridgelabz.userregistration.aopservice;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+public class AOPService {
+
+    //logs method before hitting API
+    @Before("execution(* com.bridgelabz.userregistration.controller.UserController.*(..))")
+    public void logBeforeV1(JoinPoint joinPoint) {
+        log.info("Initiating API : " + joinPoint.getSignature().getName() + " ");
+    }
+
+    //logs method After hitting API
+    @After("execution(* com.bridgelabz.userregistration.controller.UserController.*(..))")
+    public void logAfter(JoinPoint joinPoint) {
+        log.info("API successfully Executed : " + joinPoint.getSignature().getName() + " ");
+    }
+
+    //logs parameters After hitting API
+    @After("execution(* com.bridgelabz.userregistration.controller.UserController.*(..))")
+    public void logAfterAndSaveArgs(JoinPoint joinPoint) {
+        log.info("After : " + Arrays.toString(joinPoint.getArgs()));
+    }
+}
